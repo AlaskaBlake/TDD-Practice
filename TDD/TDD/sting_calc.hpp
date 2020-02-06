641cb6e97a61121a46327927c8157361bff2b98a
Theng <tyang13@alaska.edu>
2020-02-05 16:29:26 -0900

    update more feature in class 2/5/20

--- TDD/TDD/sting_calc.hpp
+++ TDD/TDD/sting_calc.hpp
@@ -9,18 +9,35 @@ using std::string;
 #include <sstream>
 using std::istringstream;
 
+
 int stringCalc(string input)
 {
 	int int1 = 0;
 	int int2 = 0;
+	int holder = 0;
 	char op;
 	istringstream ins(input);
 	ins >> int1;
 	ins >> op;
+
+	if (op - '0' >= 0 && op - '0' <= 9) {
+		holder = op - '0';
+	}
+
+
 	ins >> int2;
 
-	if (op == '+')
-		int1 += int2;
+	if (int2 >= 10) {
+		int2 = holder * 100 + int2;
+	}
+	else if (int2 >= 1) {
+		int2 = holder * 10 + int2;
+	}
+	else {
+		int2 = holder;
+	}
+
+	int1 += int2;
 
 	return int1;
 }
--- TDD/TDD/test_suites.cpp
+++ TDD/TDD/test_suites.cpp
@@ -38,12 +38,58 @@ TEST_CASE("Testing integers")
 TEST_CASE("Testing simple addition")
 {
 	{
-		INFO("0+0 should return 0");
-		REQUIRE(stringCalc("0+0") == 0);
+		INFO("0,0 should return 0");
+		REQUIRE(stringCalc("0,0") == 0);
 	}
 
 	{
-		INFO("1+1 should return 1");
-		REQUIRE(stringCalc("1+1") == 2);
+		INFO("1,1 should return 2");
+		REQUIRE(stringCalc("1,1") == 2);
 	}
+
+	{
+		INFO("2,2 should return 4");
+		REQUIRE(stringCalc("2,2") == 4);
+	}
+
+	{
+		INFO("20,5 should return 25");
+		REQUIRE(stringCalc("20,5") == 25);
+	}
+
+}
+
+TEST_CASE("Testing New Line")
+{
+	{
+		INFO("0\\n0 should return 0");
+		REQUIRE(stringCalc("0\n0") == 0);
+
+	}
+
+	{
+		INFO("1\\n1 should return 2");
+		REQUIRE(stringCalc("1\n1") == 2);
+
+	}
+
+	{
+		INFO("2\\n6 should return 8");
+		REQUIRE(stringCalc("2\n6") == 8);
+
+	}
+
+	{
+		INFO("21\\n11 should return 32");
+		REQUIRE(stringCalc("21\n11") == 32);
+
+	}
+
+	{
+		INFO("101\\n110 should return 211");
+		REQUIRE(stringCalc("101\n110") == 211);
+
+	}
+
+
 }
